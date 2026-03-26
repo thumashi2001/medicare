d17ee7f4b09e0bc52afe086195a94c1af1dc1a79
Nethasa Rupasinghe <nethasarupasinghe@gmail.com>
2026-03-26 20:39:05 +0530

    Auth service basic setup with register and login

---
+++ services/auth-service/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./src/routes/authRoutes"));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Auth Service running on port ${PORT}`);
+});
