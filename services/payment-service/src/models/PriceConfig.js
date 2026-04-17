ee6df20658e65fbe829a1d5926be54d30ac0d7f7
yasanjith365 <yasanjith365@gmail.com>
2026-04-17 20:39:41 +0530

    chore: remove untracked files and fix gitignore

--- services/payment-service/src/models/PriceConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const priceConfigSchema = new mongoose.Schema({
-  amount: { type: String, required: true }, // The single value the admin defines
-  updatedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("PriceConfig", priceConfigSchema);
